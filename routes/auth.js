6f897b75b6f5c4a016e4bcff68d2d498c5971143
lucasaking <70596715+lucasaking@users.noreply.github.com>
2021-01-25 02:43:22 -0500

    re-upload

---
+++ routes/auth.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const Router = require("express").Router;
+const router = new Router();
+
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError } = require("../expressError");
+
+
+/** POST /login: {username, password} => {token} */
+
+router.post("/login", async function (req, res, next) {
+  try {
+    let { username, password } = req.body;
+    if (await User.authenticate(username, password)) {
+      let token = jwt.sign({ username }, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json({ token });
+    } else {
+      throw new UnauthorizedError("Invalid username/password");
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST /register: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ */
+
+router.post("/register", async function (req, res, next) {
+  try {
+    let { username } = await User.register(req.body);
+    let token = jwt.sign({ username }, SECRET_KEY);
+    User.updateLoginTimestamp(username);
+    return res.json({ token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
